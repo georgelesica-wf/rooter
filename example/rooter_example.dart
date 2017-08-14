7de2e71e075e5b22853f5947a5cfd0559911c409
George Lesica <george.lesica@workiva.com>
2017-08-14 17:11:39 -0600

    Initial commit from Stagehand

---
+++ example/rooter_example.dart
@@ -0,0 +1,6 @@
+import 'package:rooter/rooter.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/rooter.dart
@@ -0,0 +1,5 @@
+library rooter;
+
+export 'src/rooter_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
